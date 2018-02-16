42d31568e48e77bc0c4531b3f0066d6783714f23
Ritesh Singh <1124ritesh@gmail.com>
2018-02-16 12:25:29 +0530

    fixed permission issue

--- windows/javascript.bat
+++ windows/javascript.bat
@@ -2,22 +2,22 @@
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install nodejs.install
+choco install nodejs.install -y
 
-choco install gulp-cli
+choco install gulp-cli -y
 
-npm install -g gulp
+npm install -g gulp -y
 
-npm install -g bower
+npm install -g bower -y
 
-npm install -g webpack
+npm install -g webpack -y
 
-choco install yarn
+choco install yarn -y
 
-choco install sublimetext3
+choco install sublimetext3 -y
 
-choco install googlechrome
+choco install googlechrome -y
 
-choco install sqlite
+choco install sqlite -y
 
 @pause
