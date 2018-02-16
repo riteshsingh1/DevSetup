e756c8e6ecf37b455ac4e69832c206bf8631982e
Ritesh Singh <1124ritesh@gmail.com>
2018-02-16 12:24:35 +0530

    updated permission issue

--- windows/php.bat
+++ windows/php.bat
@@ -2,18 +2,18 @@
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install bitnami-xampp
+choco install bitnami-xampp -y
 
-choco install composer
+choco install composer -y
 
-choco install nodejs.install
+choco install nodejs.install -y
 
-choco install yarn
+choco install yarn -y
 
-choco install sublimetext3
+choco install sublimetext3 -y
 
-choco install googlechrome
+choco install googlechrome -y
 
-choco install sqlite
+choco install sqlite -y
 
 @pause
