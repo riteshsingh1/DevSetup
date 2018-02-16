fc260e2daa4abd3e0a73268dba10191a1741396d
Ritesh Singh <1124ritesh@gmail.com>
2018-02-16 12:26:13 +0530

    fixed permission issue

--- windows/android.bat
+++ windows/android.bat
@@ -3,14 +3,14 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 
-choco install jdk8
+choco install jdk8 -y
 
-choco install androidstudio
+choco install androidstudio -y
 
-choco install sublimetext3
+choco install sublimetext3 -y
 
-choco install googlechrome
+choco install googlechrome -y
 
-choco install sqlite
+choco install sqlite -y
 
 @pause
